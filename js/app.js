b05d85818357c67c7579862d2817a34264a8177d
6erik <estanton@csumb.edu>
2021-02-03 20:20:35 -0800

    Added headers in ajax call

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ $( document ).ready(function() {
         type: 'GET',
         dataType: 'json',
         contentType: "application/json",
+        headers: {
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            },
         success: function(result){
             console.log(result);
         }
