526efc73a16af2964746bea2896310f647b811bf
6erik <estanton@csumb.edu>
2021-02-03 20:56:14 -0800

    Changed ajax call

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 $( document ).ready(function() {
 
-    $.ajax({
-        url: "https://scores.weaklytyped.com/api/v1/sports/nba/events",
-        type: "GET",
-        dataType: "json",
-        crossDomain: true,
-        success: function(result){
-            console.log(result);
-        }
-    }); 
+    var url = "https://scores.weaklytyped.com/api/v1/sports/nba/events";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }};
+
+    xhr.send();
+
 
 });
