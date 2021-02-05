af7114d299146b0404b8cf267acf0cf31b15ac9f
6erik <estanton@csumb.edu>
2021-02-04 20:16:19 -0800

    Added button to list NCAAM games

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 });
 
 
-$('#search').click(function() {
+$('#listNBA').click(function() {
     const url2 = "https://cors-anywhere.herokuapp.com/";
 
     $.ajax({
@@ -31,7 +31,41 @@ $('#search').click(function() {
                         }
                     }
                     if(str != ""){
-                        $("#table").append(str).removeClass("hidden");
+                        $("#tableNBA").append(str).removeClass("hidden");
+                    }
+                }
+                console.log(result);
+            },
+        error: function(error){
+            console.log(error);
+        }
+    });
+});
+
+$('#listNCAAM').click(function() {
+    const url2 = "https://cors-anywhere.herokuapp.com/";
+
+    $.ajax({
+        url: url2 + "https://scores.weaklytyped.com/api/v1/sports/ncaam/events",
+        crossDomain: "false",
+        success: function(result){
+            if (result) {
+                var len = result.scores.length;
+                var str = "";
+
+                if(len > 0){
+                    for(var i = 0; i < len; i++){
+                        let game = result.scores[0].shortName;
+                        let awayScore = result.scores[0].teams.awayTeam.score;
+                        let homeScore = result.scores[0].teams.homeTeam.score;
+
+                        console.log(game + " " + awayScore + " " + homeScore);
+
+                            str += "<tr><td>" + game + "</td><td>" + awayScore + "</td><td>" + homeScore + "</td></tr>";
+                        }
+                    }
+                    if(str != ""){
+                        $("#tableNCAAM").append(str).removeClass("hidden");
                     }
                 }
                 console.log(result);
